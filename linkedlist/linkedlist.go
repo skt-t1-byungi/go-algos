71ad4b89de90c3f3bda25f65a3fb11b2887a7cd1
skt-t1-byungi <tiniwz@gmail.com>
2019-05-06 23:17:11 +0900

    rewrite linkedlist to doubly

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -2,104 +2,190 @@ package linkedlist
 
 type LinkedList struct {
 	head *Node
+	tail *Node
 	len  int
 }
 
 type Node struct {
+	prev  *Node
 	next  *Node
 	value interface{}
 }
 
 func New() *LinkedList {
-	return &LinkedList{nil, 0}
+	return &LinkedList{nil, nil, 0}
 }
 
-func (ll *LinkedList) Append(val interface{}) {
-	ll.InsertAt(ll.len, val)
+func (ll *LinkedList) each(iteratee func(node *Node)) {
+	for node := ll.head; node != nil; node = node.next {
+		iteratee(node)
+	}
 }
 
-func (ll *LinkedList) InsertAt(at int, val interface{}) bool {
-	if at < 0 || at > ll.len {
-		return false
-	}
-
-	var ptr **Node
-	if at == 0 {
-		ptr = &ll.head
-	} else {
-		ptr = &ll.nodeAt(at - 1).next
-	}
-
-	curr := &Node{nil, val}
-	if next := *ptr; next != nil {
-		curr.next = next
-	}
-	*ptr = curr
-	ll.len++
-
-	return true
+func (ll LinkedList) Slice() []interface{} {
+	s := make([]interface{}, ll.len)
+	i := -1
+	ll.each(func(node *Node) {
+		i++
+		s[i] = node.value
+	})
+	return s
 }
 
-func (ll *LinkedList) nodeAt(at int) *Node {
-	curr := ll.head
-	for ; at > 0; at-- {
-		curr = curr.next
-	}
-	return curr
-}
-
-func (ll *LinkedList) At(at int) (interface{}, bool) {
-	if ll.isOutOfBounds(at) {
-		return nil, false
-	}
-	return ll.nodeAt(at).value, true
-}
-
-func (ll *LinkedList) isOutOfBounds(i int) bool {
-	return i < 0 || i >= ll.len
-}
-
-func (ll *LinkedList) RemoveAt(at int) bool {
-	if ll.isOutOfBounds(at) {
-		return false
-	}
-
-	var ptr **Node
-	if at == 0 {
-		ptr = &ll.head
-	} else {
-		ptr = &ll.nodeAt(at - 1).next
-	}
-
-	var tail *Node
-	if curr := *ptr; curr.next != nil {
-		tail = curr.next
-		curr.next = nil
-	}
-	*ptr = tail
-	ll.len--
-
-	return true
-}
-
-func (ll *LinkedList) Size() int {
+func (ll *LinkedList) Len() int {
 	return ll.len
 }
 
-func (ll *LinkedList) Each(iteratee func(val interface{}, i int)) {
-	curr := ll.head
-	for i := 0; i < ll.len; i++ {
-		iteratee(curr.value, i)
-		curr = curr.next
+func (ll *LinkedList) Append(val interface{}) {
+	newNode := &Node{nil, nil, val}
+	var ptr **Node
+	if ll.len == 0 {
+		ptr = &ll.head
+	} else {
+		ptr = &ll.tail.next
+		newNode.prev = ll.tail
+	}
+	*ptr = newNode
+	ll.tail = newNode
+	ll.len++
+}
+
+func (ll *LinkedList) Prepend(val interface{}) {
+	if ll.len == 0 {
+		ll.Append(val)
+	} else {
+		prevHead := ll.head
+		ll.head = &Node{nil, prevHead, val}
+		prevHead.prev = ll.head
+		ll.len++
 	}
 }
 
-func (ll *LinkedList) Slice() []interface{} {
-	r := make([]interface{}, ll.Size())
-	ll.Each(func(val interface{}, i int) {
-		r[i] = val
+func (ll *LinkedList) At(idx int) interface{} {
+	node := ll.nodeAt(idx)
+	if node != nil {
+		return node.value
+	}
+	return nil
+}
+
+func (ll *LinkedList) First() interface{} {
+	return ll.At(0)
+}
+
+func (ll *LinkedList) Last() interface{} {
+	return ll.At(ll.len - 1)
+}
+
+func (ll *LinkedList) nodeAt(idx int) *Node {
+	if idx < 0 || idx >= ll.len {
+		return nil
+	}
+
+	var node *Node
+	if idx < ll.len>>1 {
+		node = ll.head
+		for i := 0; i < idx; i++ {
+			node = node.next
+		}
+	} else {
+		node = ll.tail
+		for i := ll.len - 1; i > idx; i-- {
+			node = node.prev
+		}
+	}
+	return node
+}
+
+func (ll *LinkedList) InsertAt(idx int, val interface{}) {
+	idx = withNegativeIndex(ll.len, idx)
+
+	if idx == 0 {
+		ll.Prepend(val)
+		return
+	} else if idx == ll.len {
+		ll.Append(val)
+		return
+	}
+
+	if idx > ll.len {
+		for i := ll.len; i < idx; i++ {
+			ll.Append(nil)
+		}
+		ll.Append(val)
+		return
+	}
+
+	oldNode := ll.nodeAt(idx)
+	prevNode := oldNode.prev
+	newNode := &Node{prevNode, oldNode, val}
+	oldNode.prev = newNode
+	prevNode.next = newNode
+	ll.len++
+}
+
+func withNegativeIndex(len, idx int) int {
+	if idx < 0 {
+		if len == 0 {
+			idx = 0
+		} else {
+			idx = len + idx%len
+		}
+	}
+	return idx
+}
+
+func (ll *LinkedList) RemoveFirst() bool {
+	return ll.removeEdge(func() {
+		newHead := ll.head.next
+		ll.head.next = nil
+		newHead.prev = nil
+		ll.head = newHead
 	})
-	return r
+}
+
+func (ll *LinkedList) RemoveLast() bool {
+	return ll.removeEdge(func() {
+		newTail := ll.tail.prev
+		ll.tail.prev = nil
+		newTail.next = nil
+		ll.tail = newTail
+	})
+}
+
+func (ll *LinkedList) removeEdge(remover func()) bool {
+	if ll.len == 0 {
+		return false
+	}
+	if ll.len == 1 {
+		ll.head = nil
+		ll.tail = nil
+	} else {
+		remover()
+	}
+	ll.len--
+	return true
+}
+
+func (ll *LinkedList) RemoveAt(idx int) bool {
+	idx = withNegativeIndex(ll.len, idx)
+	if idx > ll.len-1 {
+		return false
+	}
+
+	if idx == 0 {
+		return ll.RemoveFirst()
+	}
+	if idx == ll.len-1 {
+		return ll.RemoveLast()
+	}
+
+	node := ll.nodeAt(idx)
+	prevNode, nextNode := node.prev, node.next
+	prevNode.next, nextNode.prev = nextNode, prevNode
+	node.prev, node.next = nil, nil
+	ll.len--
+	return true
 }
 
 func (ll *LinkedList) Find(finder func(val interface{}, i int) bool) (interface{}, int) {
--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -1,67 +1,163 @@
 package linkedlist
 
 import (
-	"reflect"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
-func TestLinkedList(t *testing.T) {
+func ExampleLinkedList_Append() {
 	ll := New()
-	if ll.Size() != 0 {
-		t.Errorf("Expected 0, but %d", ll.Size())
-	}
-
 	ll.Append(3)
-	ll.Append(1)
 	ll.Append(2)
-	if v, _ := ll.At(0); v.(int) != 3 {
-		t.Errorf("Expected 3, but %d", v)
-	}
+	ll.Append(5)
+	printList(ll)
+	// output:
+	// 3,nil,2
+	// 2,3,5
+	// 5,2,nil
+}
 
-	if v, _ := ll.At(1); v.(int) != 1 {
-		t.Errorf("Expected 1, but %d", v)
-	}
+func ExampleLinkedList_Prepend() {
+	ll := New()
+	ll.Prepend(3)
+	ll.Prepend(2)
+	ll.Prepend(5)
+	printList(ll)
+	// output:
+	// 5,nil,2
+	// 2,5,3
+	// 3,2,nil
+}
 
-	if ll.Size() != 3 {
-		t.Errorf("Expected 3, but %d", ll.Size())
-	}
+func TestLinkedList_At(t *testing.T) {
+	ll := New()
+	ll.Append(3)
+	ll.Append(2)
+	ll.Append(5)
+	assert.Equal(t, 3, ll.At(0).(int))
+	assert.Equal(t, 2, ll.At(1).(int))
+	assert.Equal(t, 5, ll.At(2).(int))
+	assert.Nil(t, ll.At(-1))
+	assert.Nil(t, ll.At(3))
+	ll.Append(4)
+	assert.Equal(t, 4, ll.At(3).(int))
+}
 
-	if slice := ll.Slice(); !reflect.DeepEqual(slice, []interface{}{3, 1, 2}) {
-		t.Errorf("Expected [3 1 2], but %v", slice)
+func BenchmarkLinkedList_At(b *testing.B) {
+	prepare := func() *LinkedList {
+		ll := New()
+		for i := 0; i < 20000; i++ {
+			ll.Append(0)
+		}
+		return ll
 	}
+	b.Run("At(9999) - size:20000", func(b *testing.B) {
+		ll := prepare()
+		b.ResetTimer()
+		for i := 0; i < 10000; i++ {
+			ll.At(9999)
+		}
+	})
+	b.Run("At(19999) - size:20000", func(b *testing.B) {
+		ll := prepare()
+		b.ResetTimer()
+		for i := 0; i < 10000; i++ {
+			ll.At(19999)
+		}
+	})
+}
 
-	ll.RemoveAt(1)
-	if ll.Size() != 2 {
-		t.Errorf("Expected 2, but %d", ll.Size())
+func TestLinkedList_Slice(t *testing.T) {
+	ll := New()
+	for _, n := range []int{3, 1, 2} {
+		ll.Append(n)
 	}
+	assert.Equal(t, []interface{}{3, 1, 2}, ll.Slice())
+}
 
-	if slice := ll.Slice(); !reflect.DeepEqual(slice, []interface{}{3, 2}) {
-		t.Errorf("Expected [3 2], but %v", slice)
-	}
+func TestLinkedList_InsertAt(t *testing.T) {
+	ll := New()
+	ll.InsertAt(0, 3)
+	ll.InsertAt(0, 1)
+	ll.InsertAt(-1, 2)
+	ll.InsertAt(1, 4)
+	assert.Equal(t, []interface{}{1, 4, 2, 3}, ll.Slice())
 
-	ll.InsertAt(1, 5)
-	if slice := ll.Slice(); !reflect.DeepEqual(slice, []interface{}{3, 5, 2}) {
-		t.Errorf("Expected [3 5 2], but %v", slice)
-	}
+	ll.InsertAt(7, 5)
+	assert.Equal(t, []interface{}{1, 4, 2, 3, nil, nil, nil, 5}, ll.Slice())
+}
 
-	ll.RemoveAt(0)
-	if slice := ll.Slice(); !reflect.DeepEqual(slice, []interface{}{5, 2}) {
-		t.Errorf("Expected [5 2], but %v", slice)
+func TestLinkedList_RemoveFirst(t *testing.T) {
+	ll := New()
+	assert.False(t, ll.RemoveFirst())
+	ll.Append(3)
+	ll.Append(2)
+	assert.Equal(t, 2, ll.Len())
+	assert.Equal(t, 3, ll.First().(int))
+	assert.True(t, ll.RemoveFirst())
+	assert.Equal(t, 1, ll.Len())
+	assert.Equal(t, 2, ll.First().(int))
+}
+
+func TestLinkedList_RemoveLast(t *testing.T) {
+	ll := New()
+	assert.False(t, ll.RemoveLast())
+	ll.Append(3)
+	ll.Append(2)
+	assert.Equal(t, 2, ll.Len())
+	assert.Equal(t, 2, ll.Last().(int))
+	assert.True(t, ll.RemoveLast())
+	assert.Equal(t, 1, ll.Len())
+	assert.Equal(t, 3, ll.Last().(int))
+}
+
+func TestLinkedList_RemoveAt(t *testing.T) {
+	ll := New()
+	for _, n := range []int{3, 7, 1, 2, 6, 5} {
+		ll.Append(n)
 	}
+	assert.Equal(t, []interface{}{3, 7, 1, 2, 6, 5}, ll.Slice())
+	assert.True(t, ll.RemoveAt(-2))
+	assert.Equal(t, []interface{}{3, 7, 1, 2, 5}, ll.Slice())
+	assert.True(t, ll.RemoveAt(2))
+	assert.Equal(t, []interface{}{3, 7, 2, 5}, ll.Slice())
+	assert.True(t, ll.RemoveAt(0))
+	assert.Equal(t, []interface{}{7, 2, 5}, ll.Slice())
+	assert.True(t, ll.RemoveAt(2))
+	assert.Equal(t, []interface{}{7, 2}, ll.Slice())
+	assert.False(t, ll.RemoveAt(2))
 }
 
 func TestLinkedList_Find(t *testing.T) {
-	type Data struct{ n int }
 	ll := New()
-	ll.Append(Data{8})
-	ll.Append(Data{3})
-	ll.Append(Data{10})
-
-	found, _ := ll.Find(func(val interface{}, i int) bool {
-		return val.(Data).n%2 == 1
-	})
-
-	if v := found.(Data); v.n != 3 {
-		t.Errorf("Expected 3, but %v", v.n)
+	for _, n := range []int{3, 7, 1, 2, 6, 5} {
+		ll.Append(n)
 	}
+	found, index := ll.Find(func(val interface{}, i int) bool {
+		n := val.(int)
+		return n > 5 && n < 7
+	})
+	assert.Equal(t, 6, found)
+	assert.Equal(t, 4, index)
+}
+
+func TestLinkedList_Len(t *testing.T) {
+	ll := New()
+	assert.Equal(t, 0, ll.Len())
+	ll.Append(1)
+	assert.Equal(t, 1, ll.Len())
+}
+
+func printList(ll *LinkedList) {
+	get := func(n *Node) string {
+		if n != nil {
+			return strconv.Itoa(n.value.(int))
+		}
+		return "nil"
+	}
+	ll.each(func(node *Node) {
+		fmt.Printf("%d,%s,%s\n", node.value, get(node.prev), get(node.next))
+	})
 }
